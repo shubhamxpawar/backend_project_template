a8d289e3e81a0a58c4cb8a6d046933e8adbd3d46
Shubham <realitymaniac7@gmail.com>
2025-08-14 22:39:22 +0530

    added readme

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import dotenv from "dotenv"
 import app from "./app.js"
+import connectDB from "./db/database.js";
+
 dotenv.config()
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
-    console.log(`listening on port ${port}`)
-})
+connectDB()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`listening on port ${port}`)
+        })
+    })
+    .catch((err) => {
+        console.error("MongoDB connection error : ", err)
+    })
+
+
 
